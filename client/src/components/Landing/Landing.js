f13a7a44c620fa3f181bb003278fd25d8a5f01ca
Harsh Shah <harshshah1295@gmail.com>
2022-05-04 23:40:09 -0300

    Update icon and Landing page

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,24 +1,44 @@
 import React from "react";
 import {
-    Paper
+    Paper, Typography
 } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 
 import backgroundImage from './../../assets/images/background.jpg';
+import bannerIcon from './../../assets/icons/knock-knock.svg';
 import { useSelector } from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
     root: {
         height: '100vh',
     },
+    greyLayer: {
+        backgroundColor: "black",
+        position: "absolute",
+        display: "block",
+        width: "100%",
+        height: "100%",
+        opacity: "0.5"
+    },
     image: {
-        backgroundImage: `url(${backgroundImage})`,
+        backgroundImage: `linear-gradient(0deg, rgb(0 0 0 / 65%), hsl(215deg 63% 85% / 50%)), url(${backgroundImage})`,
         backgroundRepeat: 'no-repeat',
         backgroundColor:
             theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
         backgroundSize: 'cover',
-        backgroundPosition: 'center',
         height: "inherit"
+    },
+    banner: {
+        display: "inline-flex",
+        position: "relative",
+        top: "100px",
+        left: "100px"
+    },
+    bannerIcon: {
+        backgroundImage: `url(${bannerIcon})`,
+        width: "88px",
+        height: "88px",
+        marginRight: "25px"
     },
     paperHolder: {
         display: "flex",
@@ -41,6 +61,19 @@ const Landing = (props) => {
     return (
         <section className={classes.root}>
             <section className={classes.image}>
+                <section className={classes.banner}>
+                    <section className={classes.bannerIcon}>
+                    </section>
+                    <section>
+                        <Typography variant="h3">
+                            Knock Knock
+                        </Typography>
+                        <Typography variant="h5">
+                            Let's Knock
+                        </Typography>
+                    </section>
+
+                </section>
                 {!isFetching &&
                     <Paper elevation={6} square className={classes.paperHolder}>
                         {props.children}
